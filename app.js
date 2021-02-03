20862cbf182259d25ec4eb6745076045d545135e
Renata Guedes <renataleitesg@gmail.com>
2021-02-03 18:20:43 +0000

    implements the Start Game Button and the Quit Game Button

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ new Vue({
     el: '#app',
     data: {
         running: false,
-        playerLife: 0,
+        playerLife: 100,
         monsterLife: 100,
 
     },
     computed: {
-        hasResult(){
+        hasResult() {
             return this.playerLife == 0 || this.monsterLife == 0
         }
 
     },
     methods: {
-
+        startGame() {
+            this.running = true
+            this.playerLife = 100
+            this.monsterLife = 100
+        }
     },
     watch: {
 
